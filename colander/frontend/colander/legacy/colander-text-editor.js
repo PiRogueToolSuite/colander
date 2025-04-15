2dbf0ce6180a8ef439a425391e1471df03c8d093
TontonSancho <christophe@andral.fr>
2025-04-15 10:49:43 +0200

    Fix conditional codemirror activation.

--- colander/frontend/colander/legacy/colander-text-editor.js
+++ colander/frontend/colander/legacy/colander-text-editor.js
@@ -1,66 +1,74 @@
 async function resolve_codemirror() {
-  const {
-    amy,
-    ayuLight,
-    basicSetup,
-    EditorState,
-    EditorView
-  } = await import(/* webpackChunkName: "ascodemirror" */ './async-codemirror');
-
-  function editorFromTextArea(textarea, extensions) {
-    let view = new EditorView({
-      doc: textarea.value,
-      extensions
-    });
-    $(view.dom).height('52em');
-    $(view.dom).css({'overflow': 'auto', 'max-width': '100%', 'border': '1px solid #c4c3c8', 'border-radius': '8px'});
-    textarea.parentNode.insertBefore(view.dom, textarea);
-    $($(textarea.form), $('button[type=submit]')).click(() => {
-      textarea.value = view.state.doc.toString();
-    });
-    if (textarea.form) textarea.form.addEventListener("submit", () => {
-      textarea.value = view.state.doc.toString();
-    });
-    return view;
-  }
-
-  // Replace textarea with a text editor
-  $('textarea.colander-text-editor').each(function (index, elt) {
-    const extensions = [
-      EditorView.contentAttributes.of({contenteditable: true}),
-      basicSetup,
+  const codemirror_concerns = $('textarea.colander-text-editor, pre.colander-text-editor > code');
+  if (codemirror_concerns.length > 0) {
+    const {
+      amy,
       ayuLight,
-    ];
-    editorFromTextArea(elt, extensions);
-    $(this).css('visibility', 'hidden');
-    $(this).css('position', 'absolute');
-  });
+      basicSetup,
+      EditorState,
+      EditorView
+    } = await import(/* webpackChunkName: "ascodemirror" */ './async-codemirror');
 
-  // Replace pre > code with a text editor in read-only mode
-  $('pre.colander-text-editor > code').each(function (index, elt) {
-    let view = new EditorView({
-      doc: elt.innerHTML,
-      state: EditorState.create({
+    function editorFromTextArea(textarea, extensions) {
+      let view = new EditorView({
+        doc: textarea.value,
+        extensions
+      });
+      $(view.dom).height('52em');
+      $(view.dom).css({
+        'overflow': 'auto',
+        'max-width': '100%',
+        'border': '1px solid #c4c3c8',
+        'border-radius': '8px'
+      });
+      textarea.parentNode.insertBefore(view.dom, textarea);
+      $($(textarea.form), $('button[type=submit]')).click(() => {
+        textarea.value = view.state.doc.toString();
+      });
+      if (textarea.form) textarea.form.addEventListener("submit", () => {
+        textarea.value = view.state.doc.toString();
+      });
+      return view;
+    }
+
+    // Replace textarea with a text editor
+    $('textarea.colander-text-editor').each(function (index, elt) {
+      const extensions = [
+        EditorView.contentAttributes.of({contenteditable: true}),
+        basicSetup,
+        ayuLight,
+      ];
+      editorFromTextArea(elt, extensions);
+      $(this).css('visibility', 'hidden');
+      $(this).css('position', 'absolute');
+    });
+
+    // Replace pre > code with a text editor in read-only mode
+    $('pre.colander-text-editor > code').each(function (index, elt) {
+      let view = new EditorView({
         doc: elt.innerHTML,
-        extensions: [
-          EditorView.contentAttributes.of({contenteditable: false}),
-          amy,
-          basicSetup,
-          EditorView.lineWrapping,
-        ],
-      }),
-    });
-    $(view.dom).css({
-      'max-height': '64em',
-      'overflow': 'auto',
-      'border': '1px solid #c4c3c8',
-      'border-radius': '8px'
-    });
+        state: EditorState.create({
+          doc: elt.innerHTML,
+          extensions: [
+            EditorView.contentAttributes.of({contenteditable: false}),
+            amy,
+            basicSetup,
+            EditorView.lineWrapping,
+          ],
+        }),
+      });
+      $(view.dom).css({
+        'max-height': '64em',
+        'overflow': 'auto',
+        'border': '1px solid #c4c3c8',
+        'border-radius': '8px'
+      });
 
-    elt.parentNode.parentNode.insertBefore(view.dom, elt.parentNode);
-    $(this).removeClass();
-    $(this).parent().remove();
-  });
+      elt.parentNode.parentNode.insertBefore(view.dom, elt.parentNode);
+      $(this).removeClass();
+      $(this).parent().remove();
+    });
+  }
 }
 
 export default () => {

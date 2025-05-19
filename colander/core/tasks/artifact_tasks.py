9a0eebad7a227820fc2405fc49f3d8bc29dc955f
U039b <git@0x39b.fr>
2025-05-19 13:07:00 +0200

    Use Mandolin to automatically generate the thumbnail of pictures

--- colander/core/tasks/artifact_tasks.py
+++ colander/core/tasks/artifact_tasks.py
@@ -4,12 +4,15 @@ import re
 
 import mandolin_python_client
 from django.conf import settings
+from django.core.files.base import ContentFile
 from django.utils import timezone
+from mandolin_python_client import ThumbnailStrategy
 from mandolin_python_client.rest import ApiException
 
 from elasticsearch_dsl import Index
 
 from colander.core.models import Artifact, ArtifactAnalysis
+
 logger = logging.getLogger(__name__)
 
 
@@ -25,10 +28,62 @@ def remove_duplicates_regex(s):
     return s
 
 
+def _mandolin_thumbnail(artifact: Artifact, mandolin_configuration):
+    if not artifact.mime_type.startswith('image'):
+        return
+    with NamedTemporaryFile(suffix=artifact.original_name) as artifact_file:
+        for chunk in artifact.file.chunks():
+            artifact_file.write(chunk)
+        artifact_file.flush()
+        artifact_file.seek(0)
+        with mandolin_python_client.ApiClient(mandolin_configuration) as api_client:
+            api_instance = mandolin_python_client.ConvertersApi(api_client)
+            try:
+                api_response:bytearray = api_instance.generate_thumbnail_converter_thumbnail_post(
+                    artifact_file.name,
+                    strategy=ThumbnailStrategy.FIT,
+                    width=200,
+                    height=200,
+                    _request_timeout=30,
+                )
+                if len(api_response) > 0:
+                    artifact.thumbnail = ContentFile(api_response, name=artifact_file.name)
+                    artifact.save()
+            except ApiException as e:
+                logger.error(e)
+
+
+def _mandolin_tika_analysis(artifact: Artifact, mandolin_configuration) -> ArtifactAnalysis | None:
+    with NamedTemporaryFile(suffix=artifact.original_name) as artifact_file:
+        for chunk in artifact.file.chunks():
+            artifact_file.write(chunk)
+        artifact_file.flush()
+        artifact_file.seek(0)
+        with mandolin_python_client.ApiClient(mandolin_configuration) as api_client:
+            api_instance = mandolin_python_client.AnalyzersApi(api_client)
+            file = artifact_file.name
+            try:
+                api_response = api_instance.analyze_with_tika_analyzer_tika_post(file, _request_timeout=5 * 60)
+                analysis = ArtifactAnalysis()
+                analysis.owner = str(artifact.owner_id)
+                analysis.case_id = str(artifact.case_id)
+                analysis.artifact_id = str(artifact.id)
+                analysis.analysis_date = timezone.now()
+                analysis.content = api_response.content
+                analysis.success = api_response.success
+                analysis.processors = api_response.processors
+                return analysis
+            except ApiException as e:
+                logger.error(e)
+    return None
+
+
 def analyze_artifact(artifact_id: str):
     if not settings.USE_MANDOLIN:
         logger.info('Mandolin is disabled')
         return
+
+    # Prepare the ElasticSearch index in which the analysis will be saved
     from elasticsearch_dsl import connections
     connections.create_connection(hosts=['elasticsearch'], timeout=20)
     artifact = Artifact.objects.get(id=artifact_id)
@@ -45,25 +100,11 @@ def analyze_artifact(artifact_id: str):
         return
 
     # Call Mandolin to extract the artifact content
-    configuration = mandolin_python_client.Configuration(
+    mandolin_configuration = mandolin_python_client.Configuration(
         host=settings.MANDOLIN_BASE_URL
     )
-    with NamedTemporaryFile() as artifact_file:
-        for chunk in artifact.file.chunks():
-            artifact_file.write(chunk)
-        with mandolin_python_client.ApiClient(configuration) as api_client:
-            api_instance = mandolin_python_client.AnalyzersApi(api_client)
-            file = artifact_file.name
-            try:
-                api_response = api_instance.analyze_with_tika_analyzer_tika_post(file, _request_timeout=5*50)
-                analysis = ArtifactAnalysis()
-                analysis.owner = str(artifact.owner_id)
-                analysis.case_id = str(artifact.case_id)
-                analysis.artifact_id = str(artifact.id)
-                analysis.analysis_date = timezone.now()
-                analysis.content = remove_duplicates_regex(api_response.content)
-                analysis.success = api_response.success
-                analysis.processors = api_response.processors
-                analysis.save(index=index_name)
-            except ApiException as e:
-                logger.error(e)
+    if analysis := _mandolin_tika_analysis(artifact, mandolin_configuration):
+        analysis.save(index=index_name)
+
+    # Call Mandolin to automatically generate the thumbnail of pictures
+    _mandolin_thumbnail(artifact, mandolin_configuration)

67e8e51ef87385e4fcc42e2761304fcd8750be9a
U039b <esther@pts-project.org>
2025-10-06 16:21:47 +0200

    HotFix: Reduce database connection max age to improve resource management
    Signed-off-by: U039b <esther@pts-project.org>

--- config/settings/production.py
+++ config/settings/production.py
@@ -12,7 +12,7 @@ ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
 # ------------------------------------------------------------------------------
 DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
-DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
+DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=3)  # noqa F405
 
 # CACHES
 # ------------------------------------------------------------------------------

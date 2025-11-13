322f905e25d3c92d6dc5228db68b960ae94513d0
TontonSancho <christophe@andral.fr>
2025-11-13 17:13:30 +0100

    Large file uploader refactor.

--- colander/frontend/colander-vue-components/ArtifactUploader.vue
+++ colander/frontend/colander-vue-components/ArtifactUploader.vue
@@ -2,7 +2,7 @@
 
 import FileUpload from './legacy/file_upload';
 
-function register_legacy_uploader(case_id) {
+function register_legacy_uploader(case_id, csrf_token) {
 
   if ( $('#id_upload_request_ref').val()?.trim() ) {
       $('#upload_btn').prop('disabled', true);
@@ -24,21 +24,62 @@ function register_legacy_uploader(case_id) {
     $('#artifact-upload-form').submit();
   }
 
-  function upload_error(data) {
+  function upload_error(err) {
     $('#id_file').prop('disabled', false);
     $('#artifact-upload-form .disableable').prop('disabled', false);
     $('#upload_btn').prop('disabled', false);
-    console.error('upload_error', data);
-    if (typeof data === 'string') {
-      alert(data);
+
+    if (err instanceof Error) {
+      alert(err.message);
+    }
+    else if (typeof err === 'string') {
+      alert(err);
     }
     else {
       alert('Unable to upload your file. Please refresh this page and contact your administrator.');
     }
   }
 
-  function upload_progress(data, sent, total) {
-    console.log('upload_progress', arguments);
+  let jProgress = null;
+  function create_progress_bar() {
+    jProgress = $(
+      `<div class="file-details">
+          <div class="progress" style="margin-top: 5px;">
+              <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0;">
+              </div>
+          </div>
+          <span class="file-upload-status text-muted"></span>
+      </div>`);
+    let jProgressContainer = $(`<div class="mb-2"></div>`);
+    jProgressContainer.append(jProgress);
+    jProgressContainer.insertAfter($('#id_file'));
+  }
+
+  function upload_progress(state, data, sent, total) {
+    if (state === FileUpload.State.VALIDATED) {
+      console.log('Validated ...');
+      $('#id_file').prop('disabled', true);
+    }
+    if (state === FileUpload.State.INITIALIZING) {
+      console.log('Initializing ...');
+      create_progress_bar();
+    }
+    if (state === FileUpload.State.HASHING) {
+      console.log('Hashing ...');
+      jProgress.find('.file-upload-status').text('Hashing');
+    }
+    if (state === FileUpload.State.UPLOADING) {
+      jProgress.find('.file-upload-status').text('Uploading');
+      let percent = Math.round((sent / total) * 100);
+      jProgress.find('.progress-bar').css('width', percent + '%')
+      jProgress.find('.progress-bar').text(percent + '%');
+    }
+    if (state === FileUpload.State.UPLOADED) {
+      jProgress.find('.file-upload-status').text('Uploaded');
+      let percent = 100;
+      jProgress.find('.progress-bar').css('width', percent + '%')
+      jProgress.find('.progress-bar').text(percent + '%');
+    }
   }
 
   $('#upload_btn').on('click', (event) => {
@@ -50,12 +91,11 @@ function register_legacy_uploader(case_id) {
     event.preventDefault();
     event.stopPropagation();
     const uploader = new FileUpload(
-      $('#id_file')[0],
-      //$('#artifact-upload-file')[0],
       upload_success, upload_progress, upload_error,
       `/ws/${ case_id }/collect/artifact/upload`,
+      csrf_token,
     );
-    uploader.upload();
+    uploader.upload($('#id_file')[0].files[0]);
   });
 }
 
@@ -63,9 +103,10 @@ function register_legacy_uploader(case_id) {
 export default {
   props: {
     dataCaseId: String,
+    dataCsrfToken: String,
   },
   mounted() {
-    register_legacy_uploader(this.dataCaseId);
+    register_legacy_uploader(this.dataCaseId, this.dataCsrfToken);
   }
 };
 </script>
--- colander/frontend/colander-vue-components/legacy/file_upload.js
+++ colander/frontend/colander-vue-components/legacy/file_upload.js
@@ -1,192 +1,195 @@
 export default class FileUpload {
-    static DEFAULT_CONFIG = {
-        file_input_elem: null,
-        upload_uri: '/upload',
-        max_chunk_length:  1 * 1024 * 1024,
-        success: () => {},
-        progress: () => {},
-        error: () => {},
-        progress_container_elem: null,
+
+  static State = Object.freeze({
+    VALIDATING: Symbol('initialization'),
+    VALIDATED: Symbol('validated'),
+    INITIALIZING: Symbol('initialization'),
+    HASHING: Symbol('hashing'),
+    HASHED: Symbol('hashed'),
+    INITIALIZED: Symbol('initialized'),
+    UPLOADING: Symbol('uploading'),
+    UPLOADED: Symbol('uploaded'),
+    ERROR: Symbol('error'),
+  });
+
+  static DEFAULT_CONFIG = {
+    csrf_token: null,
+    file_input_elem: null,
+    upload_uri: '/upload',
+    max_chunk_length:  1 * 1024 * 1024,
+    success: () => {},
+    progress: () => {},
+    error: () => {},
+  };
+
+  config = {};
+
+  /**
+   * Construct a FileUpload uploader.
+   * Can be called with inline parameters or config object directly as first parameter.
+   * config: {
+   *   success: Function({UploadRequest}),
+   *   progress: Function(FileUpload.State, {UploadRequest}, current_bytes, total_bytes),
+   *   error: Function(Error),
+   *   upload_uri: String,
+   *   csrf_token: String,
+   * }
+   * @param success_callback_or_config
+   * @param progress_callback
+   * @param error_callback
+   * @param upload_uri
+   * @param csrf_token
+   */
+  constructor(success_callback_or_config, progress_callback, error_callback, upload_uri, csrf_token) {
+    if (typeof success_callback_or_config === 'function') {
+      this._constructor_with_config({
+        csrf_token: csrf_token,
+        upload_uri: upload_uri,
+        success: success_callback_or_config,
+        progress: progress_callback,
+        error: error_callback
+      });
+    }
+    else {
+      this._constructor_with_config( success_callback_or_config );
+    }
+  }
+
+  _constructor_with_config(c) {
+    this.config = Object.assign( this.config, FileUpload.DEFAULT_CONFIG, c );
+    this.next_addr = 0;
+    this.slices = {};
+  }
+
+  upload(fileToUpload) {
+    this.validate(fileToUpload)
+      .then(this.init_file_upload.bind(this))
+      .then(() => {
+        // Upload started successfully
+        // Nothing to do
+      })
+      .catch((err) => {
+        this.config.progress(FileUpload.State.ERROR);
+        this.config.error(err);
+      });
+  }
+
+  async validate(fileToUpload) {
+    console.log('file ?', fileToUpload);
+    this.config.progress(FileUpload.State.VALIDATING);
+    if ( fileToUpload ) {
+      // pass
+    }
+    else {
+      throw new Error('Please select a file');
+    }
+    this.config.progress(FileUpload.State.VALIDATED);
+    return fileToUpload;
+  }
+
+  async init_file_upload(fileToUpload) {
+    this.config.progress(FileUpload.State.INITIALIZING);
+
+    this.file = fileToUpload;
+
+    await this.init_file_slices();
+
+    $.ajaxSetup({
+      async: true,
+      headers: {
+        "X-CSRFToken": this.config.csrf_token,
+      }
+    });
+    const payload = {
+      'file_name': this.file_name,
+      'file_size': this.file_size,
+      'chunks': this.slices,
     };
-    config = {};
-    jProgress = null;
-    constructor(input_elt_or_config, success_callback, progress_callback, error_callback, upload_url) {
-        console.log("constructor", typeof input_elt_or_config, arguments);
-        if (typeof input_elt_or_config === 'object') {
-            if (input_elt_or_config.file_input_elem) {
-                console.log("constructor", "GENERIC Constructor");
-                 this._constructor_with_config( input_elt_or_config );
-            }
-            else if ( $(input_elt_or_config).is('input[type=file]') ) {
-                console.log("constructor", "LEGACY Constructor");
-                this._constructor_with_config({
-                    file_input_elem: input_elt_or_config,
-                    upload_uri: upload_url,
-                    success: success_callback,
-                    progress: progress_callback,
-                    error: error_callback
-                });
-            }
-            else {
-                throw new Error("Please provide a file input elem or a { config } object");
-            }
-        }
-        else {
-            throw new Error("Please provide a file input elem or a { config } object");
-        }
-    }
-    _constructor_with_config(c) {
-        this.config = Object.assign( this.config, FileUpload.DEFAULT_CONFIG, c );
-        this.next_addr = 0;
-        this.slices = {};
-    }
-
-    create_progress_bar() {
-        this.jProgress = $(
-            `<div class="file-details">
-                <div class="progress" style="margin-top: 5px;">
-                    <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0;">
-                    </div>
-                </div>
-                <span class="file-upload-status text-muted"></span>
-            </div>`);
-        if (this.config.progress_container_elem) {
-            $(this.config.progress_container_elem).empty().append(this.jProgress);
-        }
-        else {
-            let jProgressContainer = $(`<div class="mb-2"></div>`);
-            jProgressContainer.append(this.jProgress);
-            jProgressContainer.insertAfter($(this.config.file_input_elem));
-        }
-        //document.getElementById('uploaded_files').innerHTML = progress
-    }
-
-    upload() {
-        this.validate();
-        this.create_progress_bar();
-        this.init_file_upload()
-            .then(() => {
-                console.log("Internal 'upload' success");
-            })
-            .catch(this.config.error);
-    }
-
-    validate() {
-        if ( this.config.file_input_elem.files &&
-            this.config.file_input_elem.files[0] ) {
-            // pass
-        }
-        else {
-            setTimeout(this.config.error.bind(null, 'Please select a file'), 0);
-            throw new Error('Please select at least a file');
-        }
-    }
-
-    handle_upload_initialization(data) {
+    await $.ajax({
+      url: `${this.config.upload_uri}`,
+      method: 'POST',
+      data: JSON.stringify(payload),
+      contentType:'application/json; charset=utf-8',
+      dataType: 'json',
+      success: (data) => {
+        this.config.progress(FileUpload.State.INITIALIZED);
+        this.upload_request = data;
         this.next_addr = data['next_addr'];
         this.upload_file();
+      }
+    });
+  }
+
+  async init_file_slices() {
+    this.config.progress(FileUpload.State.HASHING);
+
+    this.file_size = this.file.size;
+    this.file_name = this.file.name;
+    let addr = 0;
+
+    while(addr < this.file_size) {
+      let next_chunk = addr + this.config.max_chunk_length + 1 ;
+      const chunk = this.file.slice(addr, next_chunk);
+      const buf = await chunk.arrayBuffer()
+      const bin_hash = await crypto.subtle.digest('SHA-256', buf)
+      const hash_array = Array.from(new Uint8Array(bin_hash))
+      const hash = hash_array.map(b => b.toString(16).padStart(2, '0')).join('');
+      this.slices[addr] = hash;
+      addr = next_chunk;
     }
 
-    async init_file_upload() {
-        this.file = this.config.file_input_elem.files[0];
-        $(this.config.file_input_elem).prop('disabled', true);
-        await this.init_file_slices();
-        $.ajaxSetup({
-            async: true,
-            headers: {
-                "X-CSRFToken": document.querySelector('[name=csrfmiddlewaretoken]').value,
-            }
-        });
-        const payload = {
-            'file_name': this.file_name,
-            'file_size': this.file_size,
-            'chunks': this.slices,
-        };
-        await $.ajax({
-            url: `${this.config.upload_uri}`,
-            method: 'POST',
-            data: JSON.stringify(payload),
-            contentType:'application/json; charset=utf-8',
-            dataType: 'json',
-            success: (data) => {
-                this.upload_request = data;
-                this.next_addr = data['next_addr'];
-                this.upload_file();
-            }
-        })
+    this.config.progress(FileUpload.State.HASHED);
+  }
+
+  upload_file() {
+    let end;
+    let start_addr = this.next_addr;
+    const formData = new FormData();
+    let nextChunk = start_addr + this.config.max_chunk_length + 1;
+    let currentChunk = this.file.slice(start_addr, nextChunk);
+    let uploadedChunk = start_addr + currentChunk.size;
+    if (uploadedChunk >= this.file.size) {
+      end = 1;
+    } else {
+      end = 0;
     }
 
-    async init_file_slices () {
-        this.file_size = this.file.size;
-        this.file_name = this.file.name;
-        let addr = 0;
-        this.jProgress.find('.file-upload-status').text('Hashing')
-        while(addr < this.file_size) {
-            let next_chunk = addr + this.config.max_chunk_length + 1 ;
-            const chunk = this.file.slice(addr, next_chunk);
-            const buf = await chunk.arrayBuffer()
-            const bin_hash = await crypto.subtle.digest('SHA-256', buf)
-            const hash_array = Array.from(new Uint8Array(bin_hash))
-            const hash = hash_array.map(b => b.toString(16).padStart(2, '0')).join('');
-            this.slices[addr] = hash;
-            addr = next_chunk;
-        }
-    }
+    formData.append('file', currentChunk);
+    formData.append('addr', start_addr);
+    formData.append('eof', end);
 
-    upload_file() {
-        this.jProgress.find('.file-upload-status').text('Uploading')
-        let end;
-        let start_addr = this.next_addr;
-        const formData = new FormData();
-        let nextChunk = start_addr + this.config.max_chunk_length + 1;
-        let currentChunk = this.file.slice(start_addr, nextChunk);
-        let uploadedChunk = start_addr + currentChunk.size
-        if (uploadedChunk >= this.file.size) {
-            end = 1;
+    this.config.progress(FileUpload.State.UPLOADING, this.upload_request, uploadedChunk, this.file.size);
+
+    $.ajaxSetup({
+      async: true,
+      headers: {
+        "X-CSRFToken": this.config.csrf_token,
+      }
+    });
+    $.ajax({
+      url: `${this.config.upload_uri}/${this.upload_request.id}`,
+      type: 'POST',
+      dataType: 'json',
+      cache: false,
+      processData: false,
+      contentType: false,
+      data: formData,
+      error: (xhr) => {
+        this.config.progress(FileUpload.State.ERROR);
+        this.config.error(xhr);
+      },
+      success: (res) => {
+        this.upload_request = res;
+        this.next_addr = res['next_addr'];
+        if (this.next_addr > 0) {
+          // next chunk file upload
+          setTimeout( this.upload_file.bind(this), 0 );
         } else {
-            end = 0;
+          // upload complete
+          this.config.progress(FileUpload.State.UPLOADED);
+          this.config.success(this.upload_request);
         }
-        formData.append('file', currentChunk)
-        formData.append('addr', start_addr)
-        formData.append('eof', end)
-        let percent = 0
-        if (this.file.size < this.config.max_chunk_length) {
-            percent = Math.round((uploadedChunk / this.file.size) * 100);
-        } else {
-            percent = Math.round((uploadedChunk / this.file.size) * 100);
-        }
-        this.jProgress.find('.progress-bar').css('width', percent + '%')
-        this.jProgress.find('.progress-bar').text(percent + '%')
-        $.ajaxSetup({
-            async: true,
-            headers: {
-                "X-CSRFToken": document.querySelector('[name=csrfmiddlewaretoken]').value,
-            }
-        });
-        $.ajax({
-            url: `${this.config.upload_uri}/${this.upload_request.id}`,
-            type: 'POST',
-            dataType: 'json',
-            cache: false,
-            processData: false,
-            contentType: false,
-            data: formData,
-            error: (xhr) => {
-                this.config.error(xhr);
-            },
-            success: (res) => {
-                this.upload_request = res
-                this.next_addr = res['next_addr']
-                if (this.next_addr > 0) {
-                    this.config.progress(this.upload_request, uploadedChunk, this.file.size);
-                    // upload file in chunks
-                    setTimeout( this.upload_file.bind(this), 0 );
-                } else {
-                    // upload complete
-                    this.jProgress.find('.file-upload-status').text('Success!')
-                    this.config.success(this.upload_request);
-                }
-            }
-        });
-    };
+      },
+    });
+  };
 };
